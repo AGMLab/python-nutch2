16bf923eeda32815c65f462cdda5c1371d2d339d
Ahmet Emre Aladağ <emre.aladag@agmlab.com>
2013-05-14 19:09:21 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-nutch2',
+    version='0.1',
+    packages=['nutch2', 'nutch2.gui', 'nutch2.tests', 'nutch2.runner'],
+    url='https://github.com/AGMLab/python-nutch2',
+    license='Apache 2',
+    author='Ahmet Emre Aladağ, AGMLAB',
+    author_email='',
+    description='Python Monitoring tools for Nutch2'
+)
